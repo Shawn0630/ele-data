7091cc82ab4014e2725c9c34bd050642f156ad8b
Shaofeng Jiang <sf.jiang.ca@gmail.com>
2018-08-20 13:56:24 -0400

    update migration statement

--- src/main/resources/mysqlmigrations/V1__create_shop_table.sql
+++ src/main/resources/mysqlmigrations/V1__create_shop_table.sql
@@ -12,13 +12,11 @@ CREATE TABLE IF NOT EXISTS shop (
     promotion JSON
 );
 
-INSERT INTO shop (shop_id,shop_name,shop_url,date_of_registration,lat,lng,is_bird,is_insurance,is_brand,need_tip,promotion)
-VALUES ('00000000-0000-0000-0000-000000000000','Test 1','http://localhost:4000/apis/img/tarjan.jpg','2018/08/15 19:06:16',13.0412658,80.2338514,1,1,1,1,'[{"variety":"SUBTRACTION","slogan":"Deduct $20.0"}, {"variety":"SPECIAL","slogan":"$16.0 Off"}, {"variety":"DISCOUNT","slogan":"50% Off"}, {"variety":"NEW","slogan":"New User Deduct $17.0"}]');
-INSERT INTO shop (shop_id,shop_name,shop_url,date_of_registration,lat,lng,is_bird,is_insurance,is_brand,need_tip,promotion)
-VALUES ('00000000-0000-0000-0000-000000000001','Test 2','http://localhost:4000/apis/img/tarjan.jpg','2018/07/15 19:06:16',13.0412658,80.2338514,1,1,1,1,'[{"variety":"SUBTRACTION","slogan":"Deduct $20.0"}, {"variety":"SPECIAL","slogan":"$16.0 Off"}, {"variety":"DISCOUNT","slogan":"50% Off"}, {"variety":"NEW","slogan":"New User Deduct $17.0"}]');
-INSERT INTO shop (shop_id,shop_name,shop_url,date_of_registration,lat,lng,is_bird,is_insurance,is_brand,need_tip,promotion)
-VALUES ('00000000-0000-0000-0000-000000000002','Test 3','http://localhost:4000/apis/img/tarjan.jpg','2018/08/11 19:06:16',13.0412658,80.2338514,1,1,1,1,'[{"variety":"SUBTRACTION","slogan":"Deduct $20.0"}, {"variety":"SPECIAL","slogan":"$16.0 Off"}, {"variety":"DISCOUNT","slogan":"50% Off"}, {"variety":"NEW","slogan":"New User Deduct $17.0"}]');
-INSERT INTO shop (shop_id,shop_name,shop_url,date_of_registration,lat,lng,is_bird,is_insurance,is_brand,need_tip,promotion)
-VALUES ('00000000-0000-0000-0000-000000000003','Test 4','http://localhost:4000/apis/img/tarjan.jpg','2018/01/13 19:06:16',13.0412658,80.2338514,1,1,1,1,'[{"variety":"SUBTRACTION","slogan":"Deduct $20.0"}, {"variety":"SPECIAL","slogan":"$16.0 Off"}, {"variety":"DISCOUNT","slogan":"50% Off"}, {"variety":"NEW","slogan":"New User Deduct $17.0"}]');
-INSERT INTO shop (shop_id,shop_name,shop_url,date_of_registration,lat,lng,is_bird,is_insurance,is_brand,need_tip,promotion)
-VALUES ('00000000-0000-0000-0000-000000000004','Test 5','http://localhost:4000/apis/img/tarjan.jpg','2018/06/30 19:06:16',13.0412658,80.2338514,1,1,1,1,'[{"variety":"SUBTRACTION","slogan":"Deduct $20.0"}, {"variety":"SPECIAL","slogan":"$16.0 Off"}, {"variety":"DISCOUNT","slogan":"50% Off"}, {"variety":"NEW","slogan":"New User Deduct $17.0"}]');
+INSERT INTO shop
+(shop_id,shop_name,shop_url,date_of_registration,lat,lng,is_bird,is_insurance,is_brand,need_tip,promotion)
+VALUES
+('00000000-0000-0000-0000-000000000000','Test 1','http://localhost:4000/apis/img/tarjan.jpg','2018/08/15',13.0412658,80.2338514,1,1,1,1,'[{"variety":"SUBTRACTION","slogan":"Deduct $20.0"}, {"variety":"SPECIAL","slogan":"$16.0 Off"}, {"variety":"DISCOUNT","slogan":"50% Off"}, {"variety":"NEW","slogan":"New User Deduct $17.0"}]'),
+('00000000-0000-0000-0000-000000000001','Test 2','http://localhost:4000/apis/img/tarjan.jpg','2018/07/15',13.0412658,80.2338514,1,1,1,1,'[{"variety":"SUBTRACTION","slogan":"Deduct $20.0"}, {"variety":"SPECIAL","slogan":"$16.0 Off"}, {"variety":"DISCOUNT","slogan":"50% Off"}, {"variety":"NEW","slogan":"New User Deduct $17.0"}]'),
+('00000000-0000-0000-0000-000000000002','Test 3','http://localhost:4000/apis/img/tarjan.jpg','2018/08/11',13.0412658,80.2338514,1,1,1,1,'[{"variety":"SUBTRACTION","slogan":"Deduct $20.0"}, {"variety":"SPECIAL","slogan":"$16.0 Off"}, {"variety":"DISCOUNT","slogan":"50% Off"}, {"variety":"NEW","slogan":"New User Deduct $17.0"}]'),
+('00000000-0000-0000-0000-000000000003','Test 4','http://localhost:4000/apis/img/tarjan.jpg','2018/01/13',13.0412658,80.2338514,1,1,1,1,'[{"variety":"SUBTRACTION","slogan":"Deduct $20.0"}, {"variety":"SPECIAL","slogan":"$16.0 Off"}, {"variety":"DISCOUNT","slogan":"50% Off"}, {"variety":"NEW","slogan":"New User Deduct $17.0"}]'),
+('00000000-0000-0000-0000-000000000004','Test 5','http://localhost:4000/apis/img/tarjan.jpg','2018/06/30',13.0412658,80.2338514,1,1,1,1,'[{"variety":"SUBTRACTION","slogan":"Deduct $20.0"}, {"variety":"SPECIAL","slogan":"$16.0 Off"}, {"variety":"DISCOUNT","slogan":"50% Off"}, {"variety":"NEW","slogan":"New User Deduct $17.0"}]');
